debbf034b1a2e4a7107a6b40f8f93936c451cde1
TTrung <vothanhtrung9379@gmail.com>
2025-03-03 09:31:32 +0700

    add btnlogin and change label Matkhau

--- Nhom_3_kt/Nhom_3_kt/Form1.Designer.cs
+++ Nhom_3_kt/Nhom_3_kt/Form1.Designer.cs
@@ -31,49 +31,60 @@
             this.Username = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.Matkhau = new System.Windows.Forms.Label();
-            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Username
             // 
             this.Username.AutoSize = true;
-            this.Username.Location = new System.Drawing.Point(238, 130);
+            this.Username.Location = new System.Drawing.Point(268, 162);
             this.Username.Name = "Username";
-            this.Username.Size = new System.Drawing.Size(70, 16);
+            this.Username.Size = new System.Drawing.Size(83, 20);
             this.Username.TabIndex = 0;
             this.Username.Text = "Username";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(332, 127);
+            this.txtUserName.Location = new System.Drawing.Point(374, 159);
             this.txtUserName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(352, 22);
+            this.txtUserName.Size = new System.Drawing.Size(396, 26);
             this.txtUserName.TabIndex = 1;
             // 
             // Matkhau
             // 
             this.Matkhau.AutoSize = true;
-            this.Matkhau.Location = new System.Drawing.Point(238, 227);
+            this.Matkhau.Location = new System.Drawing.Point(268, 284);
             this.Matkhau.Name = "Matkhau";
-            this.Matkhau.Size = new System.Drawing.Size(62, 16);
+            this.Matkhau.Size = new System.Drawing.Size(78, 20);
             this.Matkhau.TabIndex = 0;
-            this.Matkhau.Text = "Mật Khẩu";
+            this.Matkhau.Text = "Password";
             // 
-            // txtmatkhau
+            // txtpassword
             // 
-            this.txtmatkhau.Location = new System.Drawing.Point(332, 224);
-            this.txtmatkhau.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtmatkhau.Name = "txtmatkhau";
-            this.txtmatkhau.Size = new System.Drawing.Size(352, 22);
-            this.txtmatkhau.TabIndex = 1;
+            this.txtpassword.Location = new System.Drawing.Point(374, 280);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(396, 26);
+            this.txtpassword.TabIndex = 1;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(374, 376);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(91, 48);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1066, 523);
-            this.Controls.Add(this.txtmatkhau);
+            this.ClientSize = new System.Drawing.Size(1199, 654);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtpassword);
             this.Controls.Add(this.Matkhau);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.Username);
@@ -90,7 +101,8 @@
         private System.Windows.Forms.Label Username;
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.Label Matkhau;
-        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button btnlogin;
     }
 }
 
